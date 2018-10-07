4ec5b58dff19422114ffae0f2715061133487518
Vern Francisco <avernethy@gmail.com>
2018-10-06 21:06:40 -0500

    Changed number of elements from 2->6

--- src/main.cpp
+++ src/main.cpp
@@ -103,11 +103,11 @@ int main() {
           }
                     
           Eigen::VectorXd ptsxE(6);
-          //ptsxE << ptsx_local[0], ptsx_local[1], ptsx_local[2], ptsx_local[3], ptsx_local[4], ptsx_local[5];
-          ptsxE << ptsx_local[0], ptsx_local[1];
+          ptsxE << ptsx_local[0], ptsx_local[1], ptsx_local[2], ptsx_local[3], ptsx_local[4], ptsx_local[5];
+          //ptsxE << ptsx_local[0], ptsx_local[1];
           Eigen::VectorXd ptsyE(6);
-          //ptsyE << ptsy_local[0], ptsy_local[1], ptsy_local[2], ptsy_local[3], ptsy_local[4], ptsy_local[5];
-          ptsyE <<  ptsy_local[0], ptsy_local[1];
+          ptsyE << ptsy_local[0], ptsy_local[1], ptsy_local[2], ptsy_local[3], ptsy_local[4], ptsy_local[5];
+          //ptsyE <<  ptsy_local[0], ptsy_local[1];
 
           
           
