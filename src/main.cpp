5d95996d7562443d70de851f4206e60cd5c552b5
Vern Francisco <avernethy@gmail.com>
2018-10-06 22:47:14 -0500

    assign steer to throttle

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,7 @@ int main() {
           double throttle_value;
           vector<double> outputs = mpc.Solve(state, coeffs);
           steer_value = 0;//-outputs[6]/deg2rad(25);  //minus is to the left
-          std::cout <<"Throttle: " <<outputs[7] << std::endl;
+          std::cout <<"Throttle: " <<outputs[6] << std::endl;
           throttle_value = outputs[6];
 
           json msgJson;
