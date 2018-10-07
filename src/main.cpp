f5fae668771a2e1a752b818d870c32cd3ffcdeab
Vern Francisco <avernethy@gmail.com>
2018-10-06 22:28:36 -0500

    re-enable throttle

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,7 @@ int main() {
           double throttle_value;
           vector<double> outputs = mpc.Solve(state, coeffs);
           steer_value = -outputs[6]/deg2rad(25);  //minus is to the left
-          throttle_value = 0.1;//outputs[7];
+          throttle_value = outputs[7];
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
