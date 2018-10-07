ea29de6b30db5aee88d69a2b6553cd9fe8b68d51
Vern Francisco <avernethy@gmail.com>
2018-10-06 20:52:54 -0500

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -134,8 +134,8 @@ int main() {
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
           // Otherwise the values will be in between [-deg2rad(25), deg2rad(25] instead of [-1, 1].
-          msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = throttle_value;
+          msgJson["steering_angle"] = 0.0;//steer_value;
+          msgJson["throttle"] = 0.1;//throttle_value;
 
           //Display the MPC predicted trajectory 
           vector<double> mpc_x_vals;
