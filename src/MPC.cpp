22d81d1cb1b594e425b50874447f7883485b1fd9
Vern Francisco <avernethy@gmail.com>
2018-10-08 19:52:35 -0500

    try to minimize steer

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -64,7 +64,7 @@ class FG_eval {
 
     //Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++){
-      fg[0] += CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 10*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 10*CppAD::pow(vars[a_start + t], 2);
     }
 
