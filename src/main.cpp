fb8bdc29f34bbcd79055299a98ed8de90e5d8858
Vern Francisco <avernethy@gmail.com>
2018-10-08 19:21:57 -0500

    flip sign of cte

--- src/main.cpp
+++ src/main.cpp
@@ -125,7 +125,7 @@ int main() {
           */
           auto coeffs = polyfit(ptsxE, ptsyE, 3);
           auto coeffs_g = polyfit(ptsxE_g, ptsyE_g, 3);
-          double cte = polyeval(coeffs, 0);
+          double cte = -polyeval(coeffs, 0);
           std::cout <<"CTE: " <<cte << std::endl;
           double epsi = 0;//-atan(3*coeffs[3] * ptsx_local[0] * ptsx_local[0] + 2 * coeffs[2] * ptsx_local[0] + coeffs[1]);
           //double epsi = -atan(coeffs[1]);
