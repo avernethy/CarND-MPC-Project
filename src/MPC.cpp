4be39c132c73c4714ee892ac7d57cc0d2a587ec0
Vern Francisco <avernethy@gmail.com>
2018-10-10 22:08:50 -0500

    chnge dt change N

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -6,8 +6,8 @@
 using CppAD::AD;
 
 // TODO: Set the timestep length and duration
-size_t N = 30;
-double dt = 0.02;
+size_t N = 25;
+double dt = 0.03;
 
 // This value assumes the model presented in the classroom is used.
 //
