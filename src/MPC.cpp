734e3545f8726fdaa3467198fcf6bcfd72d04cf0
Vern Francisco <avernethy@gmail.com>
2018-10-10 20:58:03 -0500

    penalized actuator use

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -57,14 +57,14 @@ class FG_eval {
     //From solution
     // The part of the cost based on the reference state.
     for (unsigned int t =0; t < N; t++) {
-      fg[0] += 1.0 * CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 0.1 * CppAD::pow(vars[cte_start + t], 2);
       fg[0] += 0.001 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow((vars[v_start + t] - ref_v), 2);
     }
 
     //Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; t++){
-      fg[0] += 1000*CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 2000*CppAD::pow(vars[delta_start + t], 2);
       fg[0] += 5*CppAD::pow(vars[a_start + t], 2);
     }
 
