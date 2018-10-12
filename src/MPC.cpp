fd0404b9f753c3e8a21fc29bf5c6ac0fbfb8890e
Vern Francisco <avernethy@gmail.com>
2018-10-12 16:37:00 -0500

    more cte for video

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -59,7 +59,7 @@ class FG_eval {
     // The part of the cost based on the reference state.
     for (unsigned int t =0; t < N; t++) {
       // tune cross track error gain: very sensitive at 0.001 increments
-      fg[0] += 0.01  * CppAD::pow(vars[cte_start  + t], 2);
+      fg[0] += 0.015  * CppAD::pow(vars[cte_start  + t], 2);
       // tune epsi gain: range is from 1 to 4 in 1 increments
       fg[0] += 4.0 * CppAD::pow(vars[epsi_start + t], 2);
       fg[0] += CppAD::pow((vars[v_start + t] - ref_v), 2);
