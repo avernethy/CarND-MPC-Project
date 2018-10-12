96858f2e3fa7abbc5ed1c53e5c2eaceeffcf3b9c
Vern Francisco <avernethy@gmail.com>
2018-10-12 16:51:06 -0500

    shorten dt

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -8,7 +8,7 @@ using CppAD::AD;
 // TODO: Set the timestep length and duration
 size_t N = 25;
 //dt is sensitive to speed
-double dt = 0.025;
+double dt = 0.015;
 
 // This value assumes the model presented in the classroom is used.
 //
